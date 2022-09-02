22360395734f4900d2508c56ac6de6ad3de63097
aaqib@guestsmiles.com <aaqib@guestsmiles.com>
2022-09-02 17:06:13 +0500

    Bucket Alpha DB Context Added

---
+++ DBSync/Models/BucketAlphaContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Meta_Nix.Models
+{
+    public partial class BucketAlphaContext : DbContext
+    {
+        public BucketAlphaContext()
+        {
+        }
+
+        public BucketAlphaContext(DbContextOptions<BucketAlphaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Advertiser> Advertisers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=DBConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Advertiser>(entity =>
+            {
+                entity.ToTable("Advertiser");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.AdvertiserId)
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.AdvertiserName)
+                    .HasMaxLength(100)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
